fd9e058a27a3dc7c0062e4963f3ac4569aa99350
Saba Gogrichiani <sabagogrichiani@gmail.com>
2025-04-24 18:25:49 +0400

    Initial Commit

---
+++ Meno/MenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenoApp.swift
+//  Meno
+//
+//  Created by Saba Gogrichiani on 24.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
